96dc9771daf814109ce4d79eb63e1409ee3b18fe
ZHBoy <17802118921@163.com>
2018-09-12 09:36:13 +0800

    引入文件

--- app/src/main/java/com/hao/scrollerview/MainActivity.kt
+++ app/src/main/java/com/hao/scrollerview/MainActivity.kt
@@ -2,11 +2,20 @@ package com.hao.scrollerview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val months = arrayOf("01月", "02月", "03月", "04月", "05月", "06月", "07月", "08月", "09月", "10月", "11月", "12月")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        picker.setData(months.toMutableList())
+        picker.setOnSelectedListener { _, position ->
+            showView.text = months[position]
+        }
     }
 }
+
